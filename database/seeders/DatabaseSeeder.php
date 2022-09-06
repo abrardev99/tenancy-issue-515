0b6c94c70e3f17be8eec2e2f44ea321e6bd5d4c5
Abrar Ahmad <abrar.dev99@gmail.com>
2022-09-06 12:45:59 +0500

    add tenancy package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tenant;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'email' => 'central@demo.com',
+            'password' => Hash::make('password')
+        ]);
+
+        DB::statement('drop database if exists tenantfoo');
+
+        $tenant1 = Tenant::create(['id' => 'foo']);
+        $tenant1->domains()->create(['domain' => 'foo']);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
